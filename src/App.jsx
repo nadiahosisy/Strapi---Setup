19e1f91d3b821d8d5253014169bb1b7bd5112257
Nadia <nadiahosisy@gmail.com>
2023-09-26 19:14:10 +0300

    added a button and and header to Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-import { Navbar } from "./components";
+import { Hero, Navbar, Sidebar, Submenu } from "./components";
 
 const App = () => {
   return (
     <main>
       <Navbar />
+      <Hero />
+      <Sidebar />
+      <Submenu />
     </main>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { FaBars } from "react-icons/fa";
+import { useGlobalContext } from "../context/Context";
 
 const Navbar = () => {
-  return <h2>Navbar</h2>;
+  const { openSidebar } = useGlobalContext();
+  return (
+    <nav>
+      <div className="nav-canter">
+        <h3 className="logo">strapi</h3>
+        <button className="toggle-btn" onClick={openSidebar}>
+          <FaBars />
+        </button>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
