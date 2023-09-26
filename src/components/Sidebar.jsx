748651c98e69c8816bfd7263e2f728d6398fe6af
Nadia <nadiahosisy@gmail.com>
2023-09-26 19:31:17 +0300

    added useGlobalContext to Sidebar component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,10 @@
 import React from "react";
+import { FaTimes } from "react-icons/fa";
+import { useGlobalContext } from "../context/Context";
+import sublinks from "../data";
 
 const Sidebar = () => {
+  const { isSidebar, closeSidebar } = useGlobalContext();
   return <h2>Sidebar</h2>;
 };
 
