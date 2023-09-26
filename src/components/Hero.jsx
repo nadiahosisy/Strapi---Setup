a8fac6c9abeb2421c7ce891fd18841dd959a2b22
Nadia <nadiahosisy@gmail.com>
2023-09-26 19:23:49 +0300

    added pargraph and headers to Hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 const Hero = () => {
-  return <h2>Hero</h2>;
+  return (
+    <div className="hero-container">
+      <div className="hero-center">
+        <h1>
+          Manage any Content <br />
+          Anywhere
+        </h1>
+        <p>
+          Strapi is the leading open-source headless CMS. It's 100%
+          JavaScript/TypeScript, fully customizable
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
