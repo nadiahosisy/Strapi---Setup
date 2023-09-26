f63e9088dc14b50ee8b83d998ff1fd5af5296272
Nadia <nadiahosisy@gmail.com>
2023-09-26 20:45:39 +0300

    added NavLinks component

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ import Navbar from "./Navbar";
 import Hero from "./Hero";
 import Sidebar from "./Sidebar";
 import Submenu from "./Submenu";
+import NavLinks from "./NavLinks";
 
-export { Navbar, Hero, Sidebar, Submenu };
+export { Navbar, Hero, Sidebar, Submenu, NavLinks };
