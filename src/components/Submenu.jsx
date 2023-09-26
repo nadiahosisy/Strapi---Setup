86b184fe3fe295ad24d0d83b657aedbab12985ca
Nadia <nadiahosisy@gmail.com>
2023-09-26 21:06:39 +0300

    fix bug

--- src/components/Submenu.jsx
+++ src/components/Submenu.jsx
@@ -1,6 +1,9 @@
-import React from "react";
-
+import { useGlobalContext } from "../context/Context";
+import sublinks from "../data";
 const Submenu = () => {
+  const { pageId } = useGlobalContext();
+  const currentPage = sublinks.find((item) => item.pageId === pageId);
+  console.log(currentPage);
   return <h2>Submenu</h2>;
 };
 
