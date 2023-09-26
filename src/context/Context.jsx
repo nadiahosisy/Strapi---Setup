545e3f501ac8c708050e2608228fa4e1e0eec647
Nadia <nadiahosisy@gmail.com>
2023-09-26 18:34:15 +0300

    added two functions openidebar and closeSidebar

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -4,4 +4,11 @@ const AppContext = createContext();
 
 export const AppProvider = ({ children }) => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const openSidebar = () => {
+    setIsSidebarOpen(true);
+  };
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  };
 };
