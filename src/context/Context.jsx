53ada5e12134e30f9098f99c0cce4a855650a90e
Nadia <nadiahosisy@gmail.com>
2023-09-26 18:31:22 +0300

    export AppProvider

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -1,1 +1,7 @@
 import { createContext, useState, useContext } from "react";
+
+const AppContext = createContext();
+
+export const AppProvider = ({ children }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+};
